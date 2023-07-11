5823a2405488e9126851ea225aba74190cba6dd3
Sharqy <pencilmagicart@gmail.com>
2023-07-11 13:39:26 +0300

    Done with the task

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-let alphaCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let alphaSmall = "abcdefghijklmnopqrstuvwxyz";
-let specChars = "!@#$%^&*-_=+~";
-let randCap = alphaCap[Math.floor(Math.random() * alphaCap.length)];
-let randSmall = alphaSmall[Math.floor(Math.random() * alphaSmall.length)];
-let randSpec = specChars[Math.floor(Math.random() * specChars.length)];
+let generateBtn = document.querySelector("button");
+let myInput = document.querySelector("input");
+let copyBtn = document.querySelector("#copy");
 
-let pool = [alphaCap, alphaSmall, specChars];
-let pass = "";
-let passLength = 16;
-for (let i = 0; i < passLength; i++) {
-  let currentType = pool[Math.floor(Math.random() * pool.length)];
-  pass += currentType[Math.floor(Math.random() * currentType.length)];
-}
-console.log(pass);
-console.log(pass.length);
+generateBtn.addEventListener("click", () => {
+  let alphaCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let alphaSmall = "abcdefghijklmnopqrstuvwxyz";
+  let specChars = "!@#$%^&*-_=+~";
+  let randCap = alphaCap[Math.floor(Math.random() * alphaCap.length)];
+  let randSmall = alphaSmall[Math.floor(Math.random() * alphaSmall.length)];
+  let randSpec = specChars[Math.floor(Math.random() * specChars.length)];
+
+  let pool = [alphaCap, alphaSmall, specChars];
+  let pass = "";
+  let passLength = 16;
+  for (let i = 0; i < passLength; i++) {
+    let currentType = pool[Math.floor(Math.random() * pool.length)];
+    pass += currentType[Math.floor(Math.random() * currentType.length)];
+  }
+  myInput.value = pass;
+});
+
+copyBtn.addEventListener("click", () => {
+  myInput.select();
+  // myInput.setSelectionRange(0,9999)
+  navigator.clipboard.writeText(myInput.value);
+});
