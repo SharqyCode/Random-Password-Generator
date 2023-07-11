312ee60f4dda7b0333cccd8147199a479c33101b
Sharqy <pencilmagicart@gmail.com>
2023-07-11 14:18:18 +0300

    Added password length feature

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 let generateBtn = document.querySelector("button");
-let myInput = document.querySelector("input");
+let myInput = document.querySelector("#passText");
 let copyBtn = document.querySelector("#copy");
+let lenInput = document.querySelector("#passLength");
 
 generateBtn.addEventListener("click", () => {
   let alphaCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let alphaSmall = "abcdefghijklmnopqrstuvwxyz";
   let specChars = "!@#$%^&*-_=+~";
-  let randCap = alphaCap[Math.floor(Math.random() * alphaCap.length)];
-  let randSmall = alphaSmall[Math.floor(Math.random() * alphaSmall.length)];
-  let randSpec = specChars[Math.floor(Math.random() * specChars.length)];
 
   let pool = [alphaCap, alphaSmall, specChars];
   let pass = "";
-  let passLength = 16;
-  for (let i = 0; i < passLength; i++) {
-    let currentType = pool[Math.floor(Math.random() * pool.length)];
-    pass += currentType[Math.floor(Math.random() * currentType.length)];
+  let passLength = Number(lenInput.value);
+  if (passLength <= 30) {
+    for (let i = 0; i < passLength; i++) {
+      let currentType = pool[Math.floor(Math.random() * pool.length)];
+      pass += currentType[Math.floor(Math.random() * currentType.length)];
+    }
+  } else {
+    alert("Max password length: 30");
   }
   myInput.value = pass;
 });
 
 copyBtn.addEventListener("click", () => {
   myInput.select();
-  // myInput.setSelectionRange(0,9999)
+  // myInput.setSelectionRange(0, 9999);
   navigator.clipboard.writeText(myInput.value);
+});
+
+lenInput.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
 });
